b20b7bf9014c29999610a0a3dd7309fc469b01fd
Adam Hay <heyadam66@gmail.com>
2019-06-21 16:06:51 -0500

    Forms input fields now empty after pushing employee object into array

--- client.js
+++ client.js
@@ -1,12 +1,16 @@
 $(document).ready(onReady);
 
+// This function runs when the page is loaded and enables numerous things to happen
 function onReady(){
     console.log('JQ');
     $('#submitEmployeeButton').on('click', addEmployeeInfo)
     
 }
 
+// This creates an array. Inside the array are employee objects that are created from the input
+// fields, and an employee object is placed inside when someone clicks submit
 let employeeList = [];
+
 
 function addEmployeeInfo(){
     console.log('Adding Employee!');
@@ -19,5 +23,9 @@ function addEmployeeInfo(){
      }
     console.log('Adding Employee:', newEmployee);
     employeeList.push(newEmployee)
-    
+    $('#firstName').val('');
+    $('#lastName').val('');
+    $('#ID').val('');
+    $('#employeeTitle').val('');
+    $('#annualSalary').val('');
 }
