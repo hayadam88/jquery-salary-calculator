99df69362dcb3dde56c56b2479508392e18a7406
Adam Hay <heyadam66@gmail.com>
2019-06-21 15:53:15 -0500

    First name and submit working and creating a new object with first name.

--- client.js
+++ client.js
@@ -2,7 +2,17 @@ $(document).ready(onReady);
 
 function onReady(){
     console.log('JQ');
+    $('#submitEmployeeButton').on('click', addEmployeeInfo)
     
 }
 
-console.log('JS');
+let employeeList = [];
+
+function addEmployeeInfo(){
+    console.log('Adding Employee!');
+     let newEmployee = {
+         Name: $('#firstName').val(),
+     }
+    console.log('Adding Employee:', newEmployee);
+    employeeList.push(newEmployee)
+}
