b97c0893d17c97cbcf8676d535f121ca051dc291
Adam Hay <heyadam66@gmail.com>
2019-06-23 16:41:13 -0500

    Changed a couple comments

--- client.js
+++ client.js
@@ -1,33 +1,29 @@
 $(document).ready(onReady);
 
 // This function runs when the page is loaded and enables numerous things to happen
-function onReady(){
+function onReady() {
     console.log('JQ');
     $('#submitEmployeeButton').on('click', addEmployeeInfo)
-    $('#tableBody').on('click', '.deleteButton', deleteEmployee)gi
-    
+    $('#tableBody').on('click', '.deleteButton', deleteEmployee)
+
 } // end onReady
 
 // This creates an array. Inside the array are employee objects that are created from the input
 // fields, and an employee object is placed inside when someone clicks submit
 let employeeList = [];
 
-// Declares salary variables globally
-let annualSalary = 0;
-let monthlySalary = 0;
-
 // This function creates an employee object from the information from the input fields and then
 // pushes that object into the employeeList array
-function addEmployeeInfo(){
+function addEmployeeInfo() {
     console.log('Adding Employee!');
     // Creates a new employee object from the input fields
-     let newEmployee = {
-         FirstName: $('#firstName').val(),
-         LastName: $('#lastName').val(),
-         ID: $('#ID').val(),
-         Title: $('#employeeTitle').val(),
-         AnnualSalary: $('#annualSalary').val()
-     }
+    let newEmployee = {
+        FirstName: $('#firstName').val(),
+        LastName: $('#lastName').val(),
+        ID: $('#ID').val(),
+        Title: $('#employeeTitle').val(),
+        AnnualSalary: $('#annualSalary').val()
+    }
     console.log('Adding Employee:', newEmployee);
     // Pushes the employee object into the employeeList array
     employeeList.push(newEmployee)
@@ -44,15 +40,17 @@ function addEmployeeInfo(){
 
 
 // This function will put the employees in a table on the DOM
-function showEmployees(event){
+function showEmployees(event) {
     console.log('In Show Employees');
     //targets the table body
     let el = $('#tableBody');
+    let annualSalary = 0;
+    let monthlySalary = 0;
     el.empty();
     for (let i = 0; i < employeeList.length; i++) {
         //append each item to the DOM
         el.append(
-        `<tr>
+            `<tr>
         <th>${employeeList[i].FirstName}</th>
         <th>${employeeList[i].LastName}</th>
         <th>${employeeList[i].ID}</th>
@@ -68,18 +66,17 @@ function showEmployees(event){
     } // end for loop
     // This calculates the monthly salary and turns the container red if over 20,000
     monthlySalary = Math.round(annualSalary / 12); // Math.round rounds the salary 
-                                                  // to the nearest whole number
+    // to the nearest whole number
     $('#totalMonthlySalary').html(monthlySalary);
-    if ( monthlySalary > 20000){
+    if (monthlySalary > 20000) {
         console.log('stooooop');
         $('#totalMonthlySalary').parent().addClass('backgroundRed');
     } // end if
 } // end showEmployees
 
-// This function will deletes the employee from the DOM
-function deleteEmployee(){
+// This function will delete the employee from the DOM
+function deleteEmployee() {
     console.log('Clicked delete employee');
     $(this).closest('tr').remove() // Couldn't figure out how to get this to work with .parent
-                                    // Kyle's mentor Madison suggested we do it this way
-    
+    // Kyle's mentor Madison suggested we do it this way
 }
